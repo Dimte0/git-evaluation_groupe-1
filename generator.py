023f074cdc4d4408476dc7f2941448752167e064
root <root@LAPTOP-N5EP4J0T>
2024-02-22 18:39:06 +0400

    Patch Erreur

--- generator.py
+++ generator.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
             #vérifie la convertion du string en int
             nb = int(sys.argv[1])
 
+            listOperator = ["+"]
+            sizeListOpe = len(listOperator)
             # Boucle la création
             for i in range (0,nb):
-                print( str(random.randint(1,1000)) + "+" + str(random.randint(1,1000)))
+                print( str(random.randint(1,1000)) + listOperator[random.randint(0, sizeListOpe-1)] + str(random.randint(1,1000)))
 
         except:
             print("Erreur : l'argument n'est pas entier")
