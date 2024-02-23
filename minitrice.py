82cef78bb05c67e579df231bbe29c32dda9e7754
mathilde <mathi.vinga@gmail.com>
2024-02-23 12:33:45 +0400

    Traitement des divisions et modification de la fonction Operation et du programme principal pour la division par 0.

--- minitrice.py
+++ minitrice.py
@@ -2,38 +2,45 @@
 
 def Operation(Operateur, Calcul):
     ope1, ope2 = Calcul.split(Operateur)
-    ope1, ope2 = int(ope1), int(ope2) #exception si le string ne peut être converti
-    
-    print(eval(Calcul))
+    ope1, ope2 = int(ope1), int(ope2)  # Exception si le string ne peut être converti
+    if Operateur == "/" and ope2 == 0:  # Cas division par 0
+        print("Division par 0")
+        raise SystemExit(1)  #On sort du programme avec le code erreur 1
+    else:
+        print(eval(Calcul))
+
+def compter_operateurs(Calcul):
+    # Compte le nombre total d'opérateurs dans l'expression
+    return sum(Calcul.count(op) for op in ["+", "/"])
 
 #Programme principale
 if __name__ == "__main__":
-
-    #init
-    while(1):
-
-        #Input Request
+    while True:
         try:
             ope_Calcul = input()
-        #Cas : Fin de lecture d'un fichier
-        except:
-            break #Rompt "while"
-        
-        #Cas : Press "ENTER" dans l'invite
-        if(ope_Calcul == ""):
-            print("Fin des Calculs :)")
-            break #Rompt "while"
 
-        list_Operation = ["+"]
-        #Vérification de présence d'
-        for i in list_Operation:
-            if(i in ope_Calcul):
-                try:
+            #Cas : Press "ENTER" dans l'invite
+            if ope_Calcul == "":
+                print("Fin des Calculs :)")
+                raise SystemExit(0) 
+            
+            # Si le nombre d'opérteur excède 1 on affiche une erreur de syntaxe
+            if compter_operateurs(ope_Calcul) > 1:
+                print("Erreur de syntaxe pour le calcul: \"" + ope_Calcul + "\"")
+                raise SystemExit(1)
+
+            #Vérification de présence d'opérateur
+            op_trouve = False
+            for i in ["+", "/"]:
+                if i in ope_Calcul: 
                     Operation(i, ope_Calcul)
-                except:
-                    print("Erreur de syntaxe pour le calcul: \""+ ope_Calcul +"\"")
-                    SystemExit(-1)
-            else:
-                print("Erreur Syntaxique dans l'opération envoyée")
-                SystemExit(-1)
-    
+                    op_trouve = True
+                    break
+
+            #Si il n'y a pas d'opérateur on affiche un message d'erreur
+            if not op_trouve:
+                print("Erreur de syntaxe pour le calcul: \""+ ope_Calcul +"\"")
+                raise SystemExit(1)
+            
+        except EOFError:
+            break
