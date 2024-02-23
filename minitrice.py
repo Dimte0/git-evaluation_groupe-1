ecbaf95d2fab7f853419d2f11898fb7123f83192
Nico <nico@adresse.com>
2024-02-23 15:37:29 +0400

    Correction bugs

--- minitrice.py
+++ minitrice.py
@@ -3,19 +3,14 @@
 def Operation(Operateur, Calcul):
     ope1, ope2 = Calcul.split(Operateur)
     ope1, ope2 = int(ope1), int(ope2)  # Exception si le string ne peut être converti
-    if Operateur == "/" and ope2 == 0:  # Cas division par 0
-        print("Division par 0")
-        raise SystemExit(1)  #On sort du programme avec le code erreur 1
-    else:
-        try:
-            print(eval(Calcul))
-        except:
-            print("Erreur de syntaxe pour le calcul: \""+ ope_Calcul +"\"")
-            SystemExit(-1)
+
+    print(eval(Calcul))
+    
+            
 
 def compter_operateurs(Calcul):
     # Compte le nombre total d'opérateurs dans l'expression
-    return sum(Calcul.count(op) for op in ["+", "/"])
+    return sum(Calcul.count(op) for op in ["+","-","/"])
 
 #Programme principale
 if __name__ == "__main__":
@@ -40,8 +35,15 @@ if __name__ == "__main__":
             for i in list_Operation:
                 if(i in ope_Calcul):
                     operatorFound = True
-
-                    Operation(i, ope_Calcul)
+                    try:
+                        Operation(i, ope_Calcul)
+                    except ZeroDivisionError:
+                        print("Division par 0")
+                        SystemExit(1)
+                    except :
+                        print("Erreur de syntaxe pour le calcul: \""+ ope_Calcul +"\"")
+                        SystemExit(-1)
+                        
                     
 
             if(operatorFound is False):
